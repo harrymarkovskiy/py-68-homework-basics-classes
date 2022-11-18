9e3be112b6545dce97354ad758e1b0424b4b2108
Harry Markovskiy <igorden@gmail.com>
2022-11-18 14:02:36 +0300

    added Lecturer class and rate_lec method

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
 class Mentor:
     def __init__(self, name, surname):
@@ -14,7 +23,7 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
-    pass
+    grades = {}
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -26,14 +35,24 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Harry', 'Markovskiy', 'male')
 best_student.courses_in_progress += ['Python']
  
-cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer = Reviewer('Christopher', 'Robin')
 cool_reviewer.courses_attached += ['Python']
+
+cool_lecturer = Lecturer('Albus', 'Dumbledore')
+cool_lecturer.courses_attached += ['Django']
+
+bad_lecturer = Lecturer('Fred', 'Krueger')
+bad_lecturer.courses_attached += ['Soft Skills']
  
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
- 
+
+print(f"\nСтудент {best_student.name} {best_student.surname}")
+print(f"Домашнюю работу проверял {cool_reviewer.name} {cool_reviewer.surname}")
 print(best_student.grades)
+
+print(f"\nЛучший лектор {cool_lecturer.name} {cool_lecturer.surname}") 
