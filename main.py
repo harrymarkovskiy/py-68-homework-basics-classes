2699ee8b363aa3786a92e230cd0a7bf9b2c16141
Harry Markovskiy <igorden@gmail.com>
2022-11-25 19:31:34 +0300

    reloaded __str__ and added average(base)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        
 
     def rate_lec(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
@@ -15,6 +16,19 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def average_calc(self):
+        for k,v in self.grades.items():
+            av = sum(v)/ float(len(v))
+            return round(av, 1)
+
+
+    def __str__(self):
+        if not isinstance(self, Student):
+            print('Not Student')
+            return
+        return f"\nИмя студента: {self.name}\nФамилия студента: {self.surname}\nСредняя оценка за домашние задания: {self.average_calc()}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}"
+
         
 class Mentor:
     def __init__(self, name, surname, courses_attached = [] ):
@@ -27,6 +41,12 @@ class Lecturer(Mentor):
         super().__init__(name, surname, courses_attached = [] )
         self.grades = {}
 
+    def __str__(self):
+        if not isinstance(self, Lecturer):
+            print('Not Lecturer')
+            return
+        return f"\nИмя лектора: {self.name}\nФамилия лектора: {self.surname}\nСредняя оценка за лекции: делаю функцию"
+
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -36,11 +56,17 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        if not isinstance(self, Reviewer):
+            print('Not Reviewer')
+            return
+        return f"\nИмя проверяющего: {self.name}\nФамилия: {self.surname}"
  
 best_student = Student('Harry', 'Markovskiy', 'male')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Django', 'Soft Skills']
-print(best_student.courses_in_progress)
+
  
 cool_reviewer = Reviewer('Christopher', 'Robin')
 cool_reviewer.courses_attached += ['Python']
@@ -52,8 +78,8 @@ bad_lecturer = Lecturer('Fred', 'Krueger')
 bad_lecturer.courses_attached += ['Soft Skills']
  
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 5)
+cool_reviewer.rate_hw(best_student, 'Python', 7)
 
 best_student.rate_lec(cool_lecturer, 'Django', 10)
 best_student.rate_lec(cool_lecturer, 'Django', 7)
@@ -62,14 +88,18 @@ best_student.rate_lec(cool_lecturer, 'Django', 7)
 best_student.rate_lec(bad_lecturer, 'Soft Skills', 1)
 best_student.rate_lec(bad_lecturer, 'Soft Skills', 3)
 
-print(f"\nСтудент {best_student.name} {best_student.surname}")
-print(f"Домашнюю работу проверял {cool_reviewer.name} {cool_reviewer.surname}")
-print(best_student.grades)
+# print(f"\nСтудент {best_student.name} {best_student.surname}, он учится на курсах {best_student.courses_in_progress}")
 
-print(f"\nЛучший лектор {cool_lecturer.name} {cool_lecturer.surname}")
-print(cool_lecturer.courses_attached)
-print(cool_lecturer.grades)
+# print(f"\nДомашнюю работу {best_student.name} {best_student.surname} проверял {cool_reviewer.name} {cool_reviewer.surname}, оценки вот такие: {best_student.grades}")
 
-print(f"\nТак себе лектор {bad_lecturer.name} {bad_lecturer.surname}")
-print(bad_lecturer.courses_attached)
-print(bad_lecturer.grades)
+# print(f"\nЛучший лектор {cool_lecturer.name} {cool_lecturer.surname} ведет курс(ы) {cool_lecturer.courses_attached} и его оценки {cool_lecturer.grades}")
+
+# print(f"\nТак себе лектор {bad_lecturer.name} {bad_lecturer.surname} ведет курс(ы) {bad_lecturer.courses_attached} и его оценки {bad_lecturer.grades}")
+
+print(cool_reviewer)
+
+print(cool_lecturer)
+
+print(bad_lecturer)
+
+print(best_student)
